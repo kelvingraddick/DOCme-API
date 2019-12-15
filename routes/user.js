cc248626a0a2a5632c0cf961d55cb456e8433457
Kelvin Graddick <kelvingraddick@kg.codes>
2019-12-14 23:15:53 -0500

    Updating 'users' route to 'user'

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Authenticator = require('../helpers/authenticator');
-var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
