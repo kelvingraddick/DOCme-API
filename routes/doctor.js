a518698a122a15f2c95f6ac911d0d4d4b74e48ae
Kelvin Graddick <kelvingraddick@kg.codes>
2020-03-03 05:35:09 -0500

    Fixing error in doctor.js

--- routes/doctor.js
+++ routes/doctor.js
@@ -11,62 +11,6 @@ router.get('/signin', async function(req, res, next) {
   res.send(JSON.stringify({ isSuccess: true, patient: authentication.patient }));
 });
 
-router.get('/:id', async function(req, res, next) {
-  var response = { isSuccess: false };
-
-	var id = req.params.id;
-
-  response.doctor = await Database.Doctor.findOne({
-    attributes: [
-      'id',
-      ['practice_id', 'practiceId'],
-			['is_approved', 'isApproved'],
-			['first_name', 'firstName'],
-			['last_name', 'lastName'],
-			['email_address', 'emailAddress'],
-			['phone_number', 'phoneNumber'],
-			['image_url', 'imageUrl'],
-			'description',
-			'gender',
-			['birth_date', 'birthDate'],
-      ['npi_number', 'npiNumber']
-    ],
-    include: [
-      { 
-        model: Database.Practice,
-        attributes: [
-          'id',
-          'name',
-          'description',
-          'website',
-          ['email_address', 'emailAddress'],
-          ['phone_number', 'phoneNumber'],
-          ['fax_number', 'faxNumber'],
-          ['address_line_1', 'addressLine1'],
-          ['address_line_2', 'addressLine2'],
-          'city',
-          'state',
-          ['postal_code', 'postalCode'],
-          ['country_code', 'countryCode'],
-          'latitude',
-          'longitude',
-          ['image_url', 'imageUrl']
-        ]
-      }
-    ],
-    where: {
-			id: id
-    }
-  }).catch((error) => {
-    response.isSuccess = false;
-    response.errorMessage = error.message;
-  });
-
-  response.isSuccess = response.doctor != null;
-
-  res.json(response);
-});
-
 router.get('/search', async function(req, res, next) {
   var response = { isSuccess: true }
 
@@ -121,6 +65,62 @@ router.get('/search', async function(req, res, next) {
     response.isSuccess = false;
     response.errorMessage = error.message;
   });
+
+  res.json(response);
+});
+
+router.get('/:id', async function(req, res, next) {
+  var response = { isSuccess: false };
+
+	var id = req.params.id;
+
+  response.doctor = await Database.Doctor.findOne({
+    attributes: [
+      'id',
+      ['practice_id', 'practiceId'],
+			['is_approved', 'isApproved'],
+			['first_name', 'firstName'],
+			['last_name', 'lastName'],
+			['email_address', 'emailAddress'],
+			['phone_number', 'phoneNumber'],
+			['image_url', 'imageUrl'],
+			'description',
+			'gender',
+			['birth_date', 'birthDate'],
+      ['npi_number', 'npiNumber']
+    ],
+    include: [
+      { 
+        model: Database.Practice,
+        attributes: [
+          'id',
+          'name',
+          'description',
+          'website',
+          ['email_address', 'emailAddress'],
+          ['phone_number', 'phoneNumber'],
+          ['fax_number', 'faxNumber'],
+          ['address_line_1', 'addressLine1'],
+          ['address_line_2', 'addressLine2'],
+          'city',
+          'state',
+          ['postal_code', 'postalCode'],
+          ['country_code', 'countryCode'],
+          'latitude',
+          'longitude',
+          ['image_url', 'imageUrl']
+        ]
+      }
+    ],
+    where: {
+			id: id
+    }
+  }).catch((error) => {
+    response.isSuccess = false;
+    response.errorMessage = error.message;
+  });
+
+  response.isSuccess = response.doctor != null;
 
   res.json(response);
 });
