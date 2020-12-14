dec1f1d3ab7b82b907f4868f0e573bb40f23295a
Kelvin Graddick <kelvingraddick@kg.codes>
2020-12-13 22:30:00 -0500

    Email templating with fields

--- helpers/email/email.js
+++ helpers/email/email.js
@@ -5,17 +5,24 @@ const Email = {
   templates: {
     WELCOME: WelcomeTemplate
   },
-  send: async function(to, subject, preheader, html) {
+  send: async function(to, subject, preheader, template, fields) {
     sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
     var data = {
       to: to,
       from: process.env.SENDGRID_EMAIL_ADDRESS,
       subject: subject,
       text: preheader,
-      html: html
+      html: _getTemplateHtml(template, fields)
     };
     return sendgrid.send(data);
   }
+};
+
+const _getTemplateHtml = function(template, fields) {
+  for (var field in fields) {
+    template = template.replace('{{ ' + field + ' }}', fields[field]);
+  }
+  return template;
 };
 
 module.exports = Email;
