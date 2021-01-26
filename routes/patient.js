8de5e733fe326dd64cdf090a6d1035f5fae63a82
Kelvin Graddick <kelvingraddick@kg.codes>
2021-01-26 08:52:57 -0500

    Patient update API endpoint

--- routes/patient.js
+++ routes/patient.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var authenticate = require('../middleware/authenticate');
 var authorize = require('../middleware/authorize');
 var jwt = require('jsonwebtoken');
-var path = require('path');
+var Sequelize = require('sequelize');
 var Database = require('../helpers/database');
 var Email = require('../helpers/email/email');
 var UserType = require('../constants/user-type');
@@ -47,6 +47,47 @@ router.post('/register', async function(req, res, next) {
 
         var token = jwt.sign({ type: UserType.PATIENT }, process.env.TOKEN_SECRET, { subject: foundPatient.id.toString(), issuer: 'DOCme', expiresIn: '90d' });
         res.json({ isSuccess: true, patient: foundPatient, token: token });
+      })
+      .catch(error => { 
+        res.json({ isSuccess: false, errorCode: ErrorType.DATABASE_PROBLEM, errorMessage: error.message });
+      });
+  }
+});
+
+router.post('/:patientId/update', authorize, async function(req, res, next) {
+  var patientId = req.params.patientId;
+  if (patientId != req.patient.id) {
+    res.sendStatus(403);
+  } else if (await Database.Patient.findOne({ where: { id: { [Sequelize.Op.ne]: patientId }, email_address: req.body.emailAddress } })) {
+    res.json({ isSuccess: false, errorCode: ErrorType.EMAIL_TAKEN, errorMessage: 'This email address is already taken.' });
+  } else {
+    var updatedPatient = {
+      is_active: true,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email_address: req.body.emailAddress,
+      password: req.body.password,
+      gender: req.body.gender,
+      race: req.body.race,
+      image_url: req.body.imageUrl
+    };
+    Database.Patient.update(updatedPatient, { where: { id: patientId } })
+      .then(async numberUpdated => {
+        console.info('Number of patients updated: ' + numberUpdated);
+
+        var foundPatient = await Database.Patient
+          .findOne({
+            where: { id: patientId },
+            attributes: DatabaseAttributes.PATIENT
+          });
+        
+        /* TODO: patient changed email
+        await Email.send(foundPatient.get().emailAddress, 'Welcome to DOCme ' + foundPatient.get().firstName + '!', 'Thank you for joining the DOCme platform', Email.templates.WELCOME_PATIENT)
+          .then(() => {}, error => console.error('Email error: ' + error.message))
+          .catch(error => console.error('Email error: ' + error.message));
+        */
+
+        res.json({ isSuccess: true, patient: foundPatient });
       })
       .catch(error => { 
         res.json({ isSuccess: false, errorCode: ErrorType.DATABASE_PROBLEM, errorMessage: error.message });
