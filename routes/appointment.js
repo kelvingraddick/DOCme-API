6d289681b627fc5cd929eaa040588894e063f907
Kelvin Graddick <kelvingraddick@kg.codes>
2021-07-31 02:41:23 -0400

    Push notifications for appointment endpoints

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Database = require('../helpers/database');
+var PushNotification = require('../helpers/push-notification');
 var Email = require('../helpers/email/email');
 var authorize = require('../middleware/authorize');
 var DatabaseAttributes = require('../constants/database-attributes');
@@ -140,6 +141,12 @@ router.post('/book', authorize, async function(req, res, next) {
       var patient = foundAppointment.get().patient.get();
       var doctor = foundAppointment.get().doctor.get();
       var practice = doctor.practice.get();
+      var timestamp = foundAppointment.get().timestamp;
+
+      await PushNotification.send((patient.firstName + ' ' + patient.lastName) + ' just booked an appointment with you at ' + timestamp + '! See more details on the DOCme app.', 'You have a new appointment!', 'doctor' + doctor.id)
+        .then(() => {}, error => console.error('Push notification error: ' + error.message))
+        .catch(error => console.error('Push notification error: ' + error.message));
+
       await Email.send(
         patient.emailAddress,
         'Your appointment is booked on DOCme!',
@@ -148,7 +155,7 @@ router.post('/book', authorize, async function(req, res, next) {
         {
           doctor_name: doctor.firstName + ' ' + doctor.lastName,
           doctor_location: practice.addressLine1 + ' ' + (practice.addressLine2 ? practice.addressLine2 + ' ' : '') + practice.city + ', ' + practice.state + ' ' + practice.postalCode,
-          date_and_time: foundAppointment.get().timestamp,
+          date_and_time: timestamp,
           reason: foundAppointment.get().specialty.get().name
         })
         .then(() => {}, error => console.error('Email error: ' + error.message))
@@ -191,6 +198,18 @@ router.post('/:appointmentId/update', authorize, async function(req, res, next) 
         }
       })
       .then(async function(appointment) {
+        var patient = appointment.get().patient.get();
+        var doctor = appointment.get().doctor.get();
+        var timestamp = appointment.get().timestamp;
+
+        await PushNotification.send('The appointment with ' + (patient.firstName + ' ' + patient.lastName) + ' at ' + timestamp + ' was updated! See more details on the DOCme app.', 'An appointment was updated!', 'doctor' + doctor.id)
+          .then(() => {}, error => console.error('Push notification error: ' + error.message))
+          .catch(error => console.error('Push notification error: ' + error.message));
+
+        await PushNotification.send('Your appointment with ' + (doctor.firstName + ' ' + doctor.lastName) + ' at ' + timestamp + ' was updated! See more details on the DOCme app.', 'An appointment was updated!', 'patient' + patient.id)
+          .then(() => {}, error => console.error('Push notification error: ' + error.message))
+          .catch(error => console.error('Push notification error: ' + error.message));
+
         res.json({ isSuccess: true, appointment: appointment });
       })
       .catch((error) => {
@@ -208,13 +227,39 @@ router.post('/:appointmentId/update', authorize, async function(req, res, next) 
 router.post('/:appointmentId/delete', authorize, async function(req, res, next) {
 	var id = req.params.appointmentId;
   Database.Appointment.findOne({
+    attributes: DatabaseAttributes.APPOINTMENT,
+    include: [
+      {
+        model: Database.Doctor,
+        attributes: DatabaseAttributes.DOCTOR
+      },
+      { 
+        model: Database.Patient,
+        attributes: DatabaseAttributes.PATIENT
+      },
+      { 
+        model: Database.Specialty
+      }
+    ],
     where: {
 			id: id
     }
   })
   .then(async function(appointment) {
     if (appointment) {
+      var patient = appointment.get().patient.get();
+      var doctor = appointment.get().doctor.get();
+      var timestamp = appointment.get().timestamp;
       await appointment.destroy();
+
+      await PushNotification.send('The appointment with ' + (patient.firstName + ' ' + patient.lastName) + ' at ' + timestamp + ' was cancelled/deleted! See more details on the DOCme app.', 'An appointment was cancelled/deleted!', 'doctor' + doctor.id)
+        .then(() => {}, error => console.error('Push notification error: ' + error.message))
+        .catch(error => console.error('Push notification error: ' + error.message));
+
+      await PushNotification.send('Your appointment with ' + (doctor.firstName + ' ' + doctor.lastName) + ' at ' + timestamp + ' was cancelled/deleted! See more details on the DOCme app.', 'An appointment was cancelled/deleted!', 'patient' + patient.id)
+        .then(() => {}, error => console.error('Push notification error: ' + error.message))
+        .catch(error => console.error('Push notification error: ' + error.message));
+
       res.json({ isSuccess: true });
     } else {
       res.json({ isSuccess: false, errorCode: ErrorType.NO_DATA_FOUND, errorMessage: 'Could not find appointment' });
